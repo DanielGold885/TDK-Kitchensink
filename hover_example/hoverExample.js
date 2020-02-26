ff216fb73721060a19b550b489e6a8a2224fd40a
DanielTestim <daniel@testim.io>
2020-02-26 14:21:19 +0200

    Added examples for using hover

--- hover_example/hoverExample.js
+++ hover_example/hoverExample.js
@@ -1,15 +1,28 @@
 'use strict';
 
-const { go, test, hover, waitForText } = require('testim');
-const { expect } = require('chai');
+const { go, it, hover, waitForText } = require('testim');
 
+// W3school locators
 const HOVER_ME_BUTTON = '#main > div.dropdown.dropdown2 > button';
-const TEXT_SHOWN_ON_HOVER = 'Link 1';
 const HOVER_MENU_ITEM = '#main > div.dropdown.dropdown2 > div > a:nth-child(1)';
 
+//npm-react locators
+const PRODUCTS_MENU = '#nav-products-link > span';
+const DROPDOWN_MENU_OPTION_1 = '#nav-pro-link'; 
 
-test('Hover Example', async () => {
-    await go('https://www.w3schools.com/howto/howto_css_dropdown.asp');
-    await hover(HOVER_ME_BUTTON);
-    await waitForText(HOVER_MENU_ITEM, TEXT_SHOWN_ON_HOVER)
+describe('Using hover examples', () => {
+
+    it('Hover Example_01', async () => {
+        await go('https://www.w3schools.com/howto/howto_css_dropdown.asp');
+        await hover(HOVER_ME_BUTTON);
+        await waitForText(HOVER_MENU_ITEM, 'Link 1')
+    });
+    
+    it('Hover Example_02', async () => {
+        await go('https://www.npmjs.com/package/react-hover-menu');
+        await hover(PRODUCTS_MENU);
+        await waitForText(DROPDOWN_MENU_OPTION_1, 'Pro')
+    });
 });
+
+
