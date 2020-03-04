b70d610deecf624ae33ea54ba6e4c3c7b0bc047c
DanielTestim <daniel@testim.io>
2020-03-04 13:46:31 +0200

    added another example

--- handling_table_of_content/tableOfContent.js
+++ handling_table_of_content/tableOfContent.js
@@ -13,12 +13,21 @@ test("Validate table contains all expected values ", async () => {
     await go('http://jsbin.testim.io/nez/6');
 
     const tableCellContent = await evaluate(() => Array.from(document.querySelectorAll('#table > tbody > tr > td')).map(e => e.innerText));
+    const assertion = tableCellContent.every((v, i) => v === listOfExpectedValues[i]);
+    expect(assertion).to.eql(true);
+});
+
+
+test("Validate table contains at least some of the expected values ", async () => {
+    await go('http://jsbin.testim.io/nez/6');
+
+    const tableCellContent = await evaluate(() => Array.from(document.querySelectorAll('#table > tbody > tr > td')).map(e => e.innerText));
     const assertion = tableCellContent.some((v, i) => v === listOfExpectedValues[i]);
     expect(assertion).to.eql(true);
 });
 
 //You can also use "chai" asserions to compare expected values with table content
-test("Validate table contains all expected values ", async () => {
+test("Validate table contains all expected values 02 ", async () => {
     await go('http://jsbin.testim.io/nez/6');
 
     const tableCellContent = await evaluate(() => Array.from(document.querySelectorAll('#table > tbody > tr > td')).map(e => e.innerText));
@@ -26,7 +35,7 @@ test("Validate table contains all expected values ", async () => {
 });
 
 //You can also do that with a for loop
-test.only("Validate table contains all expected values ", async () => {
+test("Validate table contains all expected values 03", async () => {
     await go('http://jsbin.testim.io/nez/6');
 
     const tableCellContent = await evaluate(() => Array.from(document.querySelectorAll('#table > tbody > tr > td')).map(e => e.innerText));
